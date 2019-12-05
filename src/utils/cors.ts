18fbf9946d26f0011d20520cd1e2878e57368eb8
Robert Isaac <robert.isaac@live.com>
2019-12-05 23:22:37 +0200

    fix cors

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,8 +1,5 @@
 export function CORS(req, res, next) {
-    console.log(req.app.get("env").PRODUCTION);
-    console.log(typeof req.app.get("env").PRODUCTION);
-    console.log(req.app.get("env").PRODUCTION === "false" || req.app.get("env").PRODUCTION === false);
-    if (req.app.get("env").PRODUCTION === "false") {
+    if (req.app.get("env").PRODUCTION === "false" || req.app.get("env").PRODUCTION === false) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,PATCH");
         res.header(
